1967e63cba2cc9970437782c80d7f7d96945a44b
brampling <brampling@infoblox.com>
2016-10-29 20:22:44 -0700

    Updated create

--- create_vm.py
+++ create_vm.py
@@ -6,6 +6,7 @@ import sys
 import requests
 import json
 import re
+import argparse
 from infoblox_client import connector
 from infoblox_client import objects
 from requests.auth import HTTPBasicAuth
@@ -17,16 +18,24 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 credentials = GoogleCredentials.get_application_default()
 compute = discovery.build('compute', 'v1', credentials=credentials)
 
-subnet = sys.argv[1]
-num = sys.argv[2]
+parser = argparse.ArgumentParser(
+	description='Create a number of VMs in Google Compute Engine. Google Cloud SDK must be installed and configured (gcloud init) and google-api-python-client Python library must be installed.',
+	epilog='Example: \'./create_vm.py 10.138.0.0 test test.com --count 2\' will create two VMs, test1.test.com and test2.test.com')
+parser.add_argument('subnet', nargs=1, help='Subnet in which to create the VMs')
+parser.add_argument('prefix', default='test', help='Prefix for the VM names')
+parser.add_argument('dnszone', help='DNS zone where host records will be created')
+parser.add_argument('--count', dest='num', type=int, default=1, help='Number of VMs to create')
+args=parser.parse_args()
+subnet = args.subnet[0]
+num = args.num
 
 niosip = '10.60.27.4'
 niosuser = 'admin'
 niospw = 'infoblox'
 project='mythic-brook-146218'
 zone='us-west1-a'
-nameprefix='test'
-dnszone='test.com'
+nameprefix = args.prefix
+dnszone = args.dnszone
 
 opts = {'host': niosip, 'username': niosuser, 'password': niospw}
 conn = connector.Connector(opts)
